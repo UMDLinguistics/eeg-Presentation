733c98753b09d41a67a1ebf6c674a4e552502fd3
Cybelle Smith <cybellesmith@hotmail.com>
2011-10-14 19:17:06 -0400

    got the presentation script to work for both textslides (which are read in as multiple lines of text) and ordinary stimuli, even when there are blank lines in between. Currently working on updating the system for recording responses, reaction times etc. such that nothing is written to an output file until after the experiment (to help fixing timing irregularities and code running in the background that might make our trigger times less accurate and lead to variability in the trigger delay).  Also, will need to change it such that all unnecessary output to the Matlab workscreen is repressed, since that will also slow down our script and may mess up the timing.

--- SentEEG.m
+++ SentEEG.m
@@ -29,31 +29,39 @@ function expt = SentEEG() %temporarily outputing experiment cell array for testi
 
 
 %%% Hard-coded parameters
-default_experimental_folder = '/Users/cnllab/Desktop/example_experiment/'
-beginTrigger = 254;
-questionTrigger = 253;
-button1 = KbName('f');
-button1Trigger = 251;
-button2 = KbName('j');
-button2Trigger = 252;
+
+%For coding purposes only:
+[exptPath,paramPath,par.default_experimental_folder] = deal('/Users/cnllab/Desktop/example_experiment/')
+exptFileName = 'bubbles.expt'
+paramFileName = 'bubbles.par'
+subjID = 'test'
+
+
+%To use in script:
+par.beginTrigger = 254;
+par.questionTrigger = 253;
+par.button1 = KbName('f');
+par.button1Trigger = 251;
+par.button2 = KbName('j');
+par.button2Trigger = 252;
 
 %%% Configure the data acquisition device
 
-di = DaqDeviceIndex; % the DaqDeviceIndex function returns the index of the port assigned to the daq device so you can refer to it in the rest of your script
-DaqDConfigPort(di,1,0); % this configures the daq port to either send output or receive input. the first number refers to which port of the daq device, A (0) or B (1). The second number refers to output (0) or input (1)
-DaqDOut(di,1,0); % this zeros out the trigger line to get started
+par.di = DaqDeviceIndex; % the DaqDeviceIndex function returns the index of the port assigned to the daq device so you can refer to it in the rest of your script
+DaqDConfigPort(par.di,1,0); % this configures the daq port to either send output or receive input. the first number refers to which port of the daq device, A (0) or B (1). The second number refers to output (0) or input (1)
+DaqDOut(par.di,1,0); % this zeros out the trigger line to get started
 
 
 %%% Initialize file names
 %%% Select parameter file
-[exptFileName, exptPath] = uigetfile('*.expt', 'Select experiment file',default_experimental_folder);
+%[exptFileName, exptPath] = uigetfile('*.expt', 'Select experiment file',par.default_experimental_folder);
 
-[paramFileName, paramPath] = uigetfile('*.par', 'Select parameter file',exptPath);
+%[paramFileName, paramPath] = uigetfile('*.par', 'Select parameter file',exptPath);
 
-subjID = input('Enter subject ID: ', 's');
+%subjID = input('Enter subject ID: ', 's');
 exptFilePrefix = strrep(exptFileName,'.txt','')
-logFileName = strcat(exptPath,subjID,'_',exptFilePrefix,'.log') %%logs events in same directory as stimulus file
-recFileName = strcat(exptPath,subjID,'_',exptFilePrefix,'.rec')  %%logs recording parameters in same directory as stimulus file
+logFileName = strcat(exptPath,subjID,'_',exptFilePrefix,'.log') %%logs events in same par.directory as stimulus file
+recFileName = strcat(exptPath,subjID,'_',exptFilePrefix,'.rec')  %%logs recorpar.ding parameters in same par.directory as stimulus file
 
 %%% Create log files
 
@@ -70,33 +78,35 @@ end
 fclose(fid);
 
 
-%ReadParameterFile is a special function for reading the parameters
+%ReadParameterFile is a special function for reapar.ding the parameters
 %Defined at end of script
-paramFileNameAndPath = strcat(paramPath,paramFileName)
-[exPar] = ReadParameterFile(paramFileNameAndPath)
-%exPar.toString
+paramFileNameAndPath = strcat(paramPath,paramFileName);
+par = ReadParameterFile(paramFileNameAndPath,par);
+%par.toString
 
-%ReadStimulusFile is a special function for reading in stim list. 
+%ReadStimulusFile is a special function for reapar.ding in stim list. 
 %Defined at end of this script
-exptFileNameAndPath = strcat(exptPath,exptFileName)
-expt = ReadExptFile(exptFileName,exptPath)
+exptFileNameAndPath = strcat(exptPath,exptFileName);
+expt = ReadExptFile(exptFileName,exptPath);
 
 %[stimulusMatrix,triggerMatrix, questionList] = ReadStimulusFile(exptFileNameAndPath) 
 %numItems = length(stimulusMatrix);
 %logData = zeros(numItems,10);
 
-%WriteRecFile is a special function for writing out the recording
+%WriteRecFile is a special function for writing out the recorpar.ding
 %parameters.
 %Defined at end of script
-WriteRecFile(recFileName,exPar,subjID, exptFileNameAndPath,paramFileNameAndPath);
+WriteRecFile(recFileName,par,subjID, exptFileNameAndPath,paramFileNameAndPath);
 
-%runExperiment(expt)
+results = runExperiment(expt,par)
+
+WriteLogFile(results,logFileName)
 
 end
 
 function expt = ReadExptFile(exptFileName,exptPath)
     exptFileNameAndPath = strcat(exptPath,exptFileName)
-    fprintf('Reading experiment file at:\n');
+    fprintf('Reapar.ding experiment file at:\n');
     fprintf('%s\n',exptFileNameAndPath);
     expt = {};
     exptFiles = {};
@@ -133,74 +143,108 @@ function expt = ReadExptFile(exptFileName,exptPath)
 
 end
 
-function runExperiment(expt)
+function results = runExperiment(expt,par)
+results.times = []
+results.stimuli = []
+results.triggers = {}
 % Grab a time baseline for the entire experiment and send a trigger to log
 
 baseTime = GetSecs()
-DaqDOut(di,1,beginTrigger); %Turn trigger on
-DaqDOut(di,1,0); %Turn trigger off     
+DaqDOut(par.di,1,par.beginTrigger); %Turn trigger on
+DaqDOut(par.di,1,0); %Turn trigger off     
 
 
-screenNumber = 0;
-wPtr = Screen('OpenWindow',screenNumber,0,[],32,2);
-black = BlackIndex(wPtr);
+par.screenNumber = 0;
+par.wPtr = Screen('OpenWindow',par.screenNumber,0,[],32,2);
+par.black = BlackIndex(par.wPtr);
 
+%what should we be doing with this variable?
 timeToLog = 0
 
+%why wait one second?
+WaitSecs(1);
+for i = 1:length(expt)
+    curritem = expt{i}
+    if (strcmp(class(curritem),'block'))
+        
+        results = runBlock(curritem,par,results);
+        
+    else
+        runTextSlide(curritem,par);
+    end
+    
+end
+
+sca
+end
+
+function runTextSlide(currTextSlide,par)
+Screen('TextSize',par.wPtr,par.textSize);
+DrawFormattedText(par.wPtr,currTextSlide,'center','center',WhiteIndex(par.wPtr));
+Screen('Flip',par.wPtr);
+KbStrokeWait
+end
+
+function results = runBlock(currblock,par,results)
+numItems = length(currblock.stimulusMatrix)
 for i = 1:numItems
     
-    currentItem = stimulusMatrix{i};  %This is the current item being presented
-    currentItemTriggerList = triggerMatrix{i}; %This is the current list of triggers for that item
+    currentItem = currblock.stimulusMatrix{i};  %This is the current item being presented
+    currentItemTriggerList = currblock.triggerMatrix{i}; %This is the current list of triggers for that item
     numWords = length(currentItem);    
     
-    Screen('TextSize',wPtr,exPar.textSize);
+    Screen('TextSize',par.wPtr,par.textSize);
     
-    DrawFormattedText(wPtr,'+','center','center',WhiteIndex(wPtr));
-    Screen('DrawingFinished',wPtr);
-    Screen('Flip',wPtr);
-    WaitSecs(exPar.fixDuration);
-    Screen('FillRect',wPtr,black);
-    Screen('DrawingFinished',wPtr);
-    Screen(wPtr,'Flip');
-    WaitSecs(exPar.IFI);
-    
-    
+    DrawFormattedText(par.wPtr,'+','center','center',WhiteIndex(par.wPtr));
+    Screen('DrawingFinished',par.wPtr);
+    Screen('Flip',par.wPtr);
+    WaitSecs(par.fixDuration);
+    Screen('FillRect',par.wPtr,par.black);
+    Screen('DrawingFinished',par.wPtr);
+    Screen('Flip',par.wPtr);
+    WaitSecs(par.IFI);
     for w = 1: numWords %This is the loop where we present the whole item
         currentWord = currentItem{w};
         currentTrigger = currentItemTriggerList{w};
         
-        Screen('TextSize',wPtr,exPar.textSize);%
-        DrawFormattedText(wPtr,currentWord,'center','center',WhiteIndex(wPtr));
-        Screen('DrawingFinished',wPtr);
-        timeToLog= Screen('Flip',wPtr);     
-        DaqDOut(di,1,currentTrigger); %Turn trigger on
-        DaqDOut(di,1,0); %Turn trigger off     
+        Screen('TextSize',par.wPtr,par.textSize);%
+        DrawFormattedText(par.wPtr,currentWord,'center','center',WhiteIndex(par.wPtr));
+        Screen('DrawingFinished',par.wPtr);
+        timeToLog= Screen('Flip',par.wPtr);     
+        DaqDOut(par.di,1,currentTrigger); %Turn trigger on
+        DaqDOut(par.di,1,0); %Turn trigger off     
         
         % Make timeToLog the actual time the subject saw stimuli, if
         % necessary or desirable to do so (may need to change code)
                 
         % This should be changed
-        WaitSecs(exPar.wordDuration);
-        Screen('FillRect',wPtr,black);
-        Screen(wPtr,'Flip');
-        WaitSecs(exPar.ISI);
+        WaitSecs(par.wordDuration);
+        Screen('FillRect',par.wPtr,par.black);
+        Screen('Flip',par.wPtr);
+        WaitSecs(par.ISI);
  
         % Log this trial
         %Change this so that ONLY write to files AFTER the experiment is
         %done!  That way won't mess with the experimental timing
-        WriteLogFile(logFileName, timeToLog, currentWord, currentTrigger);
+        %WriteLogFile(logFileName, timeToLog, currentWord, currentTrigger);
+        results = UpdateResults(results,timeToLog, currentWord, currentTrigger);
+        
     end
     
-    currentQuestion = questionList{i}
+    currentQuestion = currblock.questionList{i}
 
     if ~isempty(currentQuestion)
         
-        WaitSecs(exPar.IQI);
-        DrawFormattedText(wPtr,currentQuestion,'center','center',WhiteIndex(wPtr));
-        Screen('DrawingFinished',wPtr);
-        timeToLog= Screen('Flip',wPtr);  
-        DaqDOut(di,1,questionTrigger);
-        DaqDOut(di,1,0);
+        WaitSecs(par.IQI);
+        DrawFormattedText(par.wPtr,currentQuestion,'center','center',WhiteIndex(par.wPtr));
+        Screen('DrawingFinished',par.wPtr);
+        timeToLog= Screen('Flip',par.wPtr);  
+        DaqDOut(par.di,1,par.questionTrigger);
+        DaqDOut(par.di,1,0);
+        currentTriggers = [par.questionTrigger];
+        %Need to output trigger for specific question...
+        %DaqDOut(par.di,1,currQuestionTrigger);
+        %DaqDOut(par.di,1,0);
         
         % Make timeToLog the actual time the subject saw stimuli, if
         % necessary or desirable to do so (may need to change code)
@@ -208,13 +252,14 @@ for i = 1:numItems
         % Log the presentation of the response screen
         %Change this so that ONLY write to files AFTER the experiment is
         %done!  That way won't mess with the experimental timing
-        WriteLogFile(logFileName, timeToLog, '?', questionTrigger);
+        results = UpdateResults(results,timeToLog, '?', currentTriggers);
+        %WriteLogFile(logFileName, timeToLog, '?', par.questionTrigger);
 
         
         beg = GetSecs()
-        absTime = beg + exPar.qDuration;                    
+        absTime = beg + par.qDuration;                    
         [keyDetect, reactionTime, keyCode] = KbCheck(-1);
-        while ~ (keyCode(button1) |  keyCode(button2))
+        while ~ (keyCode(par.button1) |  keyCode(par.button2))
             [keyDetect,reactionTime,keyCode] = KbCheck(-1);
             if GetSecs() > absTime
                 break;
@@ -223,50 +268,60 @@ for i = 1:numItems
         reactionTime
        
        % Log the button press itself, if it happened
-        if keyCode(button1)    
-            DaqDOut(di,1,button1Trigger);
-            DaqDOut(di,1,0);
+        if keyCode(par.button1)    
+            DaqDOut(par.di,1,par.button1Trigger);
+            DaqDOut(par.di,1,0);
             response = 'f'; 
-            WriteLogFile(logFileName, reactionTime, response, 253);
+            results = UpdateResults(results,reactionTime, response, par.button1Trigger);
+            %WriteLogFile(logFileName, reactionTime, response, 253);
         end
         
-        if keyCode(button2)
-            DaqDOut(di,1,button2Trigger);
-            DaqDOut(di,1,0);
+        if keyCode(par.button2)
+            DaqDOut(par.di,1,par.button2Trigger);
+            DaqDOut(par.di,1,0);
             response = 'j'; 
-            WriteLogFile(logFileName, reactionTime, response, 254);
+            results = UpdateResults(results,reactionTime, response, par.button2Trigger);
+            %WriteLogFile(logFileName, reactionTime, response, 254);
         end
         
              
     end
     
     %%%Wait for button press to proceed
-    Screen('FillRect',wPtr,black);
-    Screen('DrawingFinished',wPtr);
-    Screen(wPtr,'Flip');
+    Screen('FillRect',par.wPtr,par.black);
+    Screen('DrawingFinished',par.wPtr);
+    Screen(par.wPtr,'Flip');
  
     KbStrokeWait
 
     
 end
 
-sca
 end
 
-
+function results = UpdateResults(results, timeToLog, currentWord, currentTriggers)
+results.times{length(results.times)+1} = timeToLog;
+results.words{length(results.times)+1} = currentWord;
+results.triggers{1,length(results.triggers)+1} = currentTriggers;
+end
 
 function expt = ReadExptSubFile(exptFile,expt)
-    fprintf('Reading file at:\n');
+    fprintf('Reapar.ding file at:\n');
     fprintf('%s\n',exptFile);
     currblock = InitBlock;
     fid = fopen(exptFile, 'r')
-    textLine = fgetl(fid);  %fgetl reads a single line from a file
+    textLine = fgets(fid);  %fgetl reads a single line from a file
     ii = 1;
-   %right now can be no blank lines -- that is a problem!
+   %with fget1 can be no blank lines -- that is a problem!
     while (-1 ~= textLine)
         C = textscan(textLine, '%q %d'); %use textscan to separate it
         ii
         numStim = length(C{1})
+        if (numStim == 0)
+            fprintf('there is a blank line')
+            textLine = fgets(fid); 
+            continue
+        end
         if strcmp(C{1}{1},'<textslide>')
             fprintf('textslide identified\n');
             if (ii > 1)
@@ -276,12 +331,8 @@ function expt = ReadExptSubFile(exptFile,expt)
             expt{1,length(expt)+1} = ReadTextSlide(textLine,fid);
             fprintf('textslide should be added\n');
         else
-          fprintf('not a text slide');
-          if (numStim < 1)
-                fprintf('no tokens identified\n');
-                continue;
-          end
-          
+          fprintf('not a text slide\n');
+
           for jj = 1:numStim        
               if strcmp(C{1}{jj},'?') 
                      currblock.questionList{ii} = C{1}{jj+1};
@@ -297,9 +348,9 @@ function expt = ReadExptSubFile(exptFile,expt)
           if ~strcmp(C{1}{jj}, '?')  %%if there's a question, the last thing in C should be '?'
               currblock.questionList{ii} = []  %%if no question, create an empty cell as a place holder
           end
+          ii = ii + 1;
         end
-        ii = ii + 1;
-        textLine = fgetl(fid);   
+        textLine = fgets(fid);   
     end
     
     expt{1,length(expt)+1} = currblock;
@@ -321,6 +372,12 @@ function textslide = ReadTextSlide(textLine,fid)
     while (-1 ~= textLine)
         fprintf('%s\n',textLine);
          C = textscan(textLine,'%q');
+         if (length(C{1}) == 0)
+             textslide = strcat(textslide,'\n');
+             ii = ii + 1;
+             textLine = fgetl(fid);
+             continue
+         end
          if strcmp(C{1}{1},'<textslide>')
              textLine = fgetl(fid);
              continue
@@ -328,14 +385,17 @@ function textslide = ReadTextSlide(textLine,fid)
          if strcmp(C{1}{1},'</textslide>')
              break;
          else
-             textslide{ii} = textLine;
+             textslide = strcat(textslide,textLine,'\n');
          end
-         textLine = fgetl(fid);
+         textLine = fgets(fid);
          ii = ii + 1;
     end
 end
 
-function WriteLogFile(logFileName,timeToLog,currentWord,currentTrigger)
+function WriteLogFile(results,logFileName)
+end
+
+function WriteLogFileOld(logFileName,timeToLog,currentWord,currentTrigger)
 
 fid = fopen(logFileName,'a');
 if fid == -1
@@ -350,7 +410,7 @@ fclose(fid);
 end
 
 
-function [exPar] = ReadParameterFile(paramFileName)
+function par = ReadParameterFile(paramFileName, par)
 
 fid = fopen(paramFileName,'rt');
 
@@ -360,16 +420,16 @@ end
 
 textLine = fgetl(fid);
 P = textscan(textLine,'%f')
-exPar.wordDuration = P{1}(1);
-exPar.ISI = P{1}(2);
-exPar.fixDuration = P{1}(3);
-exPar.IFI = P{1}(4);
-exPar.qDuration = P{1}(5);
-exPar.IQI = P{1}(6);
-exPar.ITI = P{1}(7);
-exPar.textSize = P{1}(8);
-exPar.presDelay = P{1}(9);
-exPar.toString = textLine
+par.wordDuration = P{1}(1);
+par.ISI = P{1}(2);
+par.fixDuration = P{1}(3);
+par.IFI = P{1}(4);
+par.qDuration = P{1}(5);
+par.IQI = P{1}(6);
+par.ITI = P{1}(7);
+par.textSize = P{1}(8);
+par.presDelay = P{1}(9);
+par.toString = textLine
 %wordDuration ISI fixDuration IFI qDuration IQI ITI textSize presDelay
 
 %should add other validators, e.g. should be 9 parameters
@@ -380,7 +440,7 @@ fclose(fid);
 
 end
 
-function WriteRecFile (recFileName,exPar,subjID, exptFileNameAndPath,paramFileNameAndPath)
+function WriteRecFile (recFileName,par,subjID, exptFileNameAndPath,paramFileNameAndPath)
 fid = fopen(recFileName,'a');
 if fid == -1
     error('Can not write to rec file.')
@@ -390,7 +450,7 @@ fprintf(fid,fmt,'Experiment File:',exptFileNameAndPath);
 fprintf(fid,fmt,'Parameter File:',paramFileNameAndPath);
 fprintf(fid,fmt,'Date:',datestr(now));
 fprintf(fid,fmt,'Subject ID:',subjID);
-fprintf(fid,fmt,'Parameters: ',exPar.toString);
+fprintf(fid,fmt,'Parameters: ',par.toString);
 
 fclose(fid);
 end
